f291c9f7f0bc04ddc0676eff545595b4dd05cafc
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-23 16:09:17 -0500

    drawer working in nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,56 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import Button from '@material-ui/core/Button'
+import Drawer from '@material-ui/core/Drawer'
 import './Nav.css';
 
-const styles = {
-header : {
-  backgroundColor:`green`
-}
 
-}
+class Nav extends Component {
 
-const Nav = (props) => (
-  
-  <div className="nav">
-    <header>
-    <Link to="/home">
-      <h1 className="nav-title">DREAMCATCHER  </h1>
-    </Link>
-    <div className='menu'>
-    <Button>Menu</Button>
-    </div>
-    </header>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <div>
-      <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
+  state = {
+    drawer: {
+      top: false,
+      bottom: false,
+      right: false,
+      left: false
+    }
+  }
+
+  toggleDrawer = (side, open) => {
+    console.log(`this is toggleDrawer, here are side and open`, side, open);
+    this.setState({
+      [side]: open,
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="nav">
+        <header>
+          <Link to="/home">
+            <h1 className="nav-title">DREAMCATCHER  </h1>
+          </Link>
+          <div className='menu'>
+            <Button onClick={() => this.toggleDrawer('left', true)}>Menu</Button>
+          </div>
+        </header>
+        <Drawer
+          open={this.state.left}
+          onClose={() => this.toggleDrawer('left', false)}
+          // onOpen={() => this.toggleDrawer('left', true)}
+          
+        >
+
+          <div className='navContainer'>
+            <Link className="nav-link" to="/home">
+              {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/info">
-            Info Page
+              {this.props.user.id ? 'Home' : 'Login / Register'}
+            </Link>
+            {/* Show the link to the info page and the logout button if the user is logged in */}
+            {this.props.user.id && (
+              <>
+                <Link className="nav-link" to="/info">
+                  Info
           </Link>
-          <LogOutButton className="nav-link"/>
-        </>
-      )}
-      {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        About
+                <Link onClick={() => this.props.dispatch({ type: 'LOGOUT' })} className="nav-link" to="/">
+                  Log Out
+          </Link>
+              </>
+            )}
+            {/* Always show this link since the about page is not protected */}
+            <Link className="nav-link" to="/about">
+              About
       </Link>
-    </div>
-  </div>
-);
+          </div>
+        </Drawer>
+
+      </div>
+    )
+  }
+}
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
