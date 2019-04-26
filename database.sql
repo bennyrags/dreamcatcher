6d827d1b185cdc8fe772364d5c2e96e1af536243
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-04-26 13:13:34 -0500

    chged sql tble

--- database.sql
+++ database.sql
@@ -8,3 +8,26 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "dreams" (
+    id SERIAL PRIMARY KEY,
+    user_id integer REFERENCES user(id),
+    date date NOT NULL,
+    description character varying(5000) NOT NULL,
+    score_temp integer NOT NULL,
+    score_mood integer NOT NULL,
+    img_url character varying(100)
+);
+
+CREATE TABLE themes (
+    id SERIAL PRIMARY KEY,
+    theme_name character varying(100) NOT NULL,
+    theme_description character varying(500),
+    theme_creation_date date NOT NULL,
+    user_id integer REFERENCES user(id)
+);
+
+CREATE TABLE themes_dreams (
+    theme_id integer REFERENCES themes(id) ON DELETE CASCADE,
+    dream_id integer REFERENCES dreams(id) ON DELETE CASCADE
+);
