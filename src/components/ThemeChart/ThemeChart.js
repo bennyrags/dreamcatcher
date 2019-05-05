33a65f76da95613d0b95468c148c35ec5fd8c6e7
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-05-05 13:39:45 -0500

    donut graph in themes working

--- src/components/ThemeChart/ThemeChart.js
+++ src/components/ThemeChart/ThemeChart.js
@@ -8,15 +8,10 @@ class ThemeChart extends Component {
 state = {
     data: {
         labels: [
-
         ],
         datasets: [{
             data: [],
-            // backgroundColor: [
-            // '#FF6384',
-            // '#36A2EB',
-            // '#FFCE56'
-            // ],
+            backgroundColor: [],
             // hoverBackgroundColor: [
             // '#FF6384',
             // '#36A2EB',
@@ -31,6 +26,27 @@ goToInfo = () => {
     this.props.history.push('/info')
 }
 
+randomColorGenerator = () => {
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
+updateState = (themesLabels, themesData, themesBackground) => {
+    console.log(`this is themesLabels in updateSTate`, themesLabels);
+    console.log(`this is themesData in updateSTate`, themesData);
+    
+    this.setState({
+        data: {
+        ...this.state.data,
+        labels: themesLabels,
+        datasets: [{
+        ...this.state.data.datasets,
+            data: themesData,
+            backgroundColor: themesBackground
+        }]
+    }
+    })
+console.log(`this is state in themeChart updateState funct`, this.state)
+}
 
 componentDidMount() {
     this.props.dispatch({ type: 'FETCH_THEME_COUNT', payload: this.props.user.id })
@@ -38,14 +54,21 @@ componentDidMount() {
 }
 
 componentDidUpdate(prevProps) {
-    if(this.props.themeCountReducer !== prevProps.themeCountReducer) {
-        let labels = [];
-        let data = [];
+    if(this.props.themes !== prevProps.themes) {
+        let themesLabels = [];
+        let themesData = [];
+        let themesBackground = [];
 
-        for (let theme of this.props.themeCountReducer) {
-            labels.push(theme.theme_name);
+        for (let theme of this.props.themes) {
+            themesLabels.push(theme.theme_name);
+            themesData.push(Number(theme.count));
+            themesBackground.push(`#${this.randomColorGenerator()}`);
         }
-        console.log(`labels in themeChart:`, labels)
+        console.log(`themesLabels in themeChart:`, themesLabels);
+        console.log(`themesData in themeChart:`, themesData);
+        console.log(`themesBackground in themeChart:`, themesBackground);
+        
+        this.updateState(themesLabels,themesData,themesBackground);
         //ok what am i measuring here? how many times each theme was used. where is that info? 
         //it is in the themes / dreams join table
         //SO
@@ -67,7 +90,20 @@ render() {
             <h1>Theme Chart</h1>
             </section>
             <section className='containerMiddle'>
+            <Doughnut data={this.state.data} 
+             options= {{
+                legend: {
+                    position:'bottom',
+                    labels: {
+                        fontColor: 'white'
+                    }
+                    
+                },
+                maintainAspectRatio: false,
+            }}
             
+            />
+
             </section>
             
             
@@ -93,7 +129,7 @@ render() {
 
 const mapStateToProps = state => ({
     user: state.user,
-    dreams: state.dreams
+    themes: state.themeCountReducer
   });
   
 
