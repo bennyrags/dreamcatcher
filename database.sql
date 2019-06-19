52edf988a4f32fd3b8da96276ad804c04cb5abf8
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-06-18 20:37:32 -0500

    added columns to user table in database.sql

--- database.sql
+++ database.sql
@@ -6,6 +6,9 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
+    "email" VARCHAR (100) UNIQUE NOT NULL,
+    "first_name" VARCHAR (200) NOT NULL,
+    "last_name" VARCHAR (200) NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
 
