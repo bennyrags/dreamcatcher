3611ed86336d96e9cca9d0f1175eac3ac0f2f7d1
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-06-19 19:57:54 -0500

    trying to get click outside comp to work w hooks, no dice

--- src/components/RegisterPage/RegisterAlert.js
+++ src/components/RegisterPage/RegisterAlert.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import { connect } from 'react-redux';
 
-function registerAlert(props) {
-//console.log(`this props error registraiton message`, this.props.errors.registrationMessage)
+function registerAlert(props, onChange) {
+
 console.log(`this props error registraiton message`, props.errors)
-    return (
+
+const node = useRef();
+console.log(`this is node:`, node);
+
+const [display, setDisplay] = useState(false);
+
+const handleClick = e => {
+    if (node.current.contains(e.target)) {
+        console.log(`in the e target!`);
+        
+        return;
+    };
+    console.log(`click outside firing`)
+    setDisplay(false);
+}
+
+useEffect(()=>{
+    document.addEventListener('mousedown', handleClick);
+    
+    return () => {
+        document.removeEventListener('mousedown', handleClick);
+    }
+}, []);
+
+return (
+    <div ref={node}>
+        {display &&
     <h2
+    id="alert"
     className="alert"
     role="alert"
     >
         {props.errors.registrationMessage}
     </h2>
+    }
+    </div>
+    
 )
 }
 
