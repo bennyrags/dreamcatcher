6085f8940821afad1fe744f59fd1a3d298e7ba84
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-07-01 19:16:43 -0500

    added user.js module file, tested crpyto in router

--- server/routes/forgotpassword.router.js
+++ server/routes/forgotpassword.router.js
@@ -1,12 +1,15 @@
+//const userStrategy = require('././strategies/user.strategy');
+const crypto = require ('crypto');
 const express = require('express');
 const pool = require('../modules/pool');
-//const userStrategy = require('././strategies/user.strategy');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 require('dotenv').config();
 
 
-router.post('/', rejectUnauthenticated, (req,res)=>{
+//console.log(`this is crypto.randomBytes(20)toString('hex'):`, crypto.randomBytes(20).toString('hex'));
+
+router.post('/', rejectUnauthenticated, (req,res,next)=>{
 console.log(`this is req.body.email:`, req.body.email);
     if (req.body.email==='') {
     res.status(400).send('email not in db')
