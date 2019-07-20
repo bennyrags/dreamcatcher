9aac00ff046a693c1b85fe4f347d63a43cdf5caf
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-07-20 12:44:52 -0500

    further about me edits

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -17,8 +17,8 @@ Horace The Dreamer is an app for dreamers.
       <p>
         I look at my phone a lot. It's the first thing I look at in the morning. It seemed only natural to build an app that could be used on a phone to track dreams. 
       </p>
-        <p>I tried to think of something clever.</p> 
-        <p>Ten minutes later, I asked my pal Horace if I could name the app after him. He said yes, provided I buy him dessert and listen to him describe his dreams of his desserts eating him.
+        <p>I tried to think of a clever name.</p> 
+        <p>All the clever names were taken and/or I wasn't that clever. So I asked my pal Horace if I could name the app after him. He said yes, provided I buy him dessert and listen to him describe his dreams of his desserts eating him.
       </p>
       <p>I reluctantly agreed.</p>
     </div>
