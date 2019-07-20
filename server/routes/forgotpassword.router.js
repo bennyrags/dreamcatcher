2401eaed110c6ddb879dc40a2a896b6e86a29631
BENJAMIN RAGSDALE <benjaminragsdale@BENJAMINs-MacBook-Pro.local>
2019-07-20 00:05:38 -0500

    cleaned up forgot pw router

--- server/routes/forgotpassword.router.js
+++ server/routes/forgotpassword.router.js
@@ -5,11 +5,9 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 require('dotenv').config();
-//const User = require('../sequelize');
 const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 
-//console.log(`this is crypto.randomBytes(20)toString('hex'):`, crypto.randomBytes(20).toString('hex'));
 
 const auth = {
     auth: {
@@ -18,7 +16,6 @@ const auth = {
     }
 }
  router.post('/',  (req,res,next)=>{
- //res.status(200).send(`hello, ${req.body.email}`);
  let email = req.body.email;
  pool.query(`SELECT "user"."email" FROM "user" WHERE "email" = $1`, [email])
 .then(result=>{
@@ -28,12 +25,10 @@ const auth = {
         res.status(200).send(`email not in db`);
     }
     else {
-        //res.status(200).send(`this email DOES exist`);
         const token = crypto.randomBytes(20).toString('hex');
         console.log(token);
         pool.query(`UPDATE "user" SET "resetPasswordToken" = $1, "resetPasswordExpires" = $2 WHERE "email"=$3`, [token, Date.now() + 360000, req.body.email])
         .then(result=>{
-            // res.status(200).send('code inserted into DB')
             console.log(`this is result.rows of update user pw:`, result);
             
              })
@@ -41,7 +36,7 @@ const auth = {
             res.status(500).send('500 - error inseting code into db')
             console.log(`error inserting code into db:`, error);
             })
-        ///this is where I'm going to try to get the mail to go
+     
 
         //make the url depend on whether deployed to heroku or not
         //set this HEROKU var using HEROKU CLI
@@ -73,7 +68,6 @@ const auth = {
                else {
                    console.log(`here is the response:`, response);
                    res.status(200).send(`recovery email sent`);
-                   //res.sendStatus(200);
                    
                }
             });
@@ -82,9 +76,6 @@ const auth = {
     
     
     }
-
-
-//    res.status(200).send(`this is result from db: ${result.rows}`)
 })
 .catch(err=>{
     res.sendStatus(500);
@@ -92,58 +83,6 @@ const auth = {
 })
 })
 
-// router.post('/', rejectUnauthenticated, (req,res,next)=>{
-// console.log(`this is req.body.email:`, req.body.email);
-//     if (req.body.email==='') {
-//     res.send('email not in db');
-//     res.sendStatus(400)
-// }
-// User.findOne({
-//     where: {
-//         email: req.body.email
-//     }
-// })
-// .then( user => {
-// if (user===null) {
-//     console.log(`email not in database`);
-//     res.json('User not in database')
-// }
-// else {
-//     const token = crypto.randomBytes(20).toString('hex');
-//     console.log(token);
-//    user.update({
-//        resetPasswordToken: token,
-//        resetPasswordExpires: Date.now() + 360000,
-//    }); 
-
-//    const transporter = nodemailer.createTransport(mg(auth))
-//    const mailOptions = {
-//        from: `${process.env.EMAIL_ADDRESS}`,
-//        to: `${user.email}`,
-//        subject: `Link to reset password`,
-//        text: 
-//        `You are receiving this email because you requested th reset the password for your account.\n\n`+
-//        `Please click on the following link within an hour of receiving it.\n\n`+
-//        `http://localhost:3000/reset/${token}`
-//    }
-//    console.log(`sending email`);
-//    transporter.sendMail(mailOptions, function(err, response){
-//        if (err) {
-//            console.log(`there was an error sending email:`, err);           
-//        }
-//        else {
-//            console.log(`here is the response:`, response);
-//            res.send(`recovery email sent`);
-//            res.sendStatus(200);
-           
-//        }
-//    })
-   
-// }
-// })
-
-
-// })
 
 
 module.exports = router; 
